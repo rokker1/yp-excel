158397f6f8a846699df0843638e987aa32134a2e
alexander @ubuntu-H310CH5-M2 <darkriderification@yandex.ru>
2022-09-06 21:03:17 +0500

    in work

--- ast/cell.cpp
+++ ast/cell.cpp
@@ -7,19 +7,51 @@
 
 
 // Реализуйте следующие методы
-Cell::Cell(){}
+Cell::Cell()
+    : impl_(std::make_unique<Impl>())
+{}
 
-Cell::~Cell() {}
+Cell::~Cell() {
+    Clear();
+}
 
-void Cell::Set(std::string text) {}
+void Cell::Set(std::string text) {
+    if(text.empty()) {
+        Clear();
+    }
+    if(text[0] == FORMULA_SIGN) {
+        text = text.substr(1);
+        if(text.empty()) {
+            // создать ячейку с одним символом =
+            Clear();
+        } else {
+            //  это формула - спарсить формулу
+            Clear();
+        }
+    } else if (text[0] == ESCAPE_SIGN) {
+        text = text.substr(1);
+        Clear();
+    } else {
+        // это текст
+        Clear();
+    }
+}
 
-void Cell::Clear() {}
+void Cell::Clear() {
+    impl_.reset();
+}
 
 Cell::Value Cell::GetValue() const {}
 std::string Cell::GetText() const {}
 
 class Cell::Impl {
 public:
+    Impl();
+    ~Impl();
+
+    virtual void Cell::Set(std::string text) = 0;
+    virtual Cell::Value Cell::GetValue() const = 0;
+    virtual std::string Cell::GetText() const = 0;
 
 private:
 
@@ -30,9 +62,9 @@ class Cell::EmptyImpl : public Cell::Impl {
 };
 
 class Cell::TextImpl : public Cell::Impl {
-
+    std::string text_;
 };
 
 class Cell::FormulaImpl : public Cell::Impl {
-
+    FormulaInterface formula_;
 };
