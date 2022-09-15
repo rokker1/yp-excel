ff4aa2ea5f25f9d5bb3accd645ef2abbfae35384
alexander @ubuntu-H310CH5-M2 <darkriderification@yandex.ru>
2022-09-15 21:24:15 +0500

    in progress.

--- ast/cell.cpp
+++ ast/cell.cpp
@@ -4,3 +4,121 @@
 #include <iostream>
 #include <string>
 #include <optional>
+
+Cell::Cell(Sheet& sheet) 
+    : sheet_(sheet)
+    , impl_(std::make_unique<EmptyImpl>(sheet_))
+{}
+
+Cell::~Cell() = default;
+
+void Cell::Set(std::string text) {
+    if(text.empty()) {
+        Clear();
+    }
+    // добавляется возможность индексов ячеек
+    if(text[0] == FORMULA_SIGN) {
+        if(text.size() == 1) {
+            // создать ячейку с одним символом =
+            impl_ = std::make_unique<TextImpl>(sheet_, "=");
+        } else {
+            //  это формула - спарсить формулу
+            std::unique_ptr<FormulaInterface> formula = ParseFormula(text.substr(1));
+            formula->GetReferencedCells();
+            impl_ = std::make_unique<FormulaImpl>(sheet_, std::move(formula));
+        }
+    } else if (text[0] == ESCAPE_SIGN) {
+        impl_ = std::make_unique<TextImpl>(sheet_, std::move(text));
+    } else {
+        // это текст
+        impl_ = std::make_unique<TextImpl>(sheet_, std::move(text));
+    }    
+}
+
+    class Cell::Impl {
+    public:
+        Impl(Sheet& sheet)
+            : sheet_(sheet) {}
+        
+        virtual Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+        Sheet& GetSheet() const {
+            return sheet_;
+        }
+
+        // virtual const std::set<Position> GetReferencedCells() const = 0;
+
+    protected:    
+        Sheet& sheet_;
+
+        std::optional<double> cached_value_;
+    };
+
+    class Cell::EmptyImpl : public Impl {
+    public:
+        EmptyImpl(Sheet& sheet)
+            : Impl(sheet) {}
+
+        Value GetValue() const override {
+            return {};
+        }
+        std::string GetText() const override {
+            return {};
+        }
+        // const std::set<Position> GetReferencedCells() const override {
+        //     return {};
+        // }
+    };
+
+    class Cell::TextImpl : public Impl {
+    public:
+        explicit TextImpl(Sheet& sheet, std::string text)
+            : Impl(sheet)
+            , text_(text) {}
+
+        Value GetValue() const override {
+            if(!text_.empty() && text_.at(0) == ESCAPE_SIGN) {
+                return text_.substr(1);
+            }
+            return text_;
+        }
+        std::string GetText() const override {
+            return text_;
+        }
+
+    private:
+        std::string text_;
+    };
+
+    class Cell::FormulaImpl : public Impl {
+    public:
+        explicit FormulaImpl(Sheet& sheet, std::unique_ptr<FormulaInterface> formula)
+            : Impl(sheet)
+            , formula_(std::move(formula))
+            // , referenced_cells_(formula_.get()->GetReferencedCells())
+        {}
+
+        Value GetValue() const override {
+            //FormulaInterface::Value res = formula_.get()->Evaluate(sheet_);
+            
+            formula_->Evaluate(sheet_);
+            if (std::holds_alternative<double>(res)) {
+                return std::get<double>(res);
+            } else if (std::holds_alternative<FormulaError>(res)) {
+                return std::get<FormulaError>(res);
+            } else {
+                throw std::runtime_error("e");
+            }
+        }
+
+        std::string GetText() const override {
+            std::string res = "=" + formula_.get()->GetExpression();
+            return res;
+        }
+
+
+        
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+        std::set<Position> referenced_cells_;
+    };
--- ast/cell.h
+++ ast/cell.h
@@ -2,171 +2,37 @@
 
 #include "common.h"
 #include "formula.h"
-#include <optional>
+
+#include <functional>
+#include <unordered_set>
+
+class Sheet;
 
 class Cell : public CellInterface {
 public:
+        Cell(Sheet& sheet);
+        ~Cell();
 
-    Cell(const SheetInterface& sheet) 
-        : sheet_(sheet)
-        , impl_(std::make_unique<EmptyImpl>(sheet_))
-    {
+        void Set(std::string text);
+        void Clear();
 
-    }
+        Value GetValue() const override;
+        std::string GetText() const override;
+        std::vector<Position> GetReferencedCells() const override;
 
-    Cell(const SheetInterface& sheet, std::string text) 
-        : sheet_(sheet)
-    {
-        Set(text);
-    }
-
-    void Set(std::string text) {
-        if(text.empty()) {
-            Clear();
-        }
-        // добавляется возможность индексов ячеек
-        if(text[0] == FORMULA_SIGN) {
-            if(text.size() == 1) {
-                // создать ячейку с одним символом =
-                impl_ = std::make_unique<TextImpl>(sheet_, "=");
-            } else {
-                //  это формула - спарсить формулу
-                std::unique_ptr<FormulaInterface> formula = ParseFormula(text.substr(1));
-                formula->GetReferencedCells();
-                impl_ = std::make_unique<FormulaImpl>(sheet_, std::move(formula));
-            }
-        } else if (text[0] == ESCAPE_SIGN) {
-            impl_ = std::make_unique<TextImpl>(sheet_, std::move(text));
-        } else {
-            // это текст
-            impl_ = std::make_unique<TextImpl>(sheet_, std::move(text));
-        }
-    }
-
-    static void InvalidateCache(Cell* cell);
-    void dfs(const CellInterface& cell); // deep-first search
-    bool CheckDependentCycles() const;
-    bool CheckReferencedCycles() const;
-
-    // Возвращает видимое значение ячейки.
-    // В случае текстовой ячейки это её текст (без экранирующих символов). В
-    // случае формулы - числовое значение формулы или сообщение об ошибке.
-    Value GetValue() const override {
-        return impl_.get()->GetValue();
-    }
-
-    // Возвращает внутренний текст ячейки, как если бы мы начали её
-    // редактирование. В случае текстовой ячейки это её текст (возможно,
-    // содержащий экранирующие символы). В случае формулы - её выражение.
-    std::string GetText() const override {
-        return impl_.get()->GetText();
-    }
-
-    void Clear() {
-        impl_ = std::make_unique<EmptyImpl>(sheet_);
-    }
-
-    std::vector<Position> GetReferencedCells() const {
-        // заглушка. реализовать позже
-        return referenced_cells_;
-    }
-
-    const SheetInterface& sheet_;
-
-    const SheetInterface& GetSheet() const {
-        return sheet_;
-    }
+        bool IsReferenced() const;
 
 private:
+        class Impl;
+        class EmptyImpl;
+        class TextImpl;
+        class FormulaImpl;
+        Sheet& sheet_;
+        std::unique_ptr<Impl> impl_;
 
-    class Impl {
-    public:
-        Impl(const SheetInterface& sheet)
-            : sheet_(sheet) {}
-        
-        virtual Value GetValue() const = 0;
-        virtual std::string GetText() const = 0;
-        const SheetInterface& GetSheet() const {
-            return sheet_;
-        }
-
-        // virtual const std::set<Position> GetReferencedCells() const = 0;
-
-    private:    
-        const SheetInterface& sheet_;
-
-        std::optional<double> cached_value_;
-    };
-
-    class EmptyImpl : public Impl {
-    public:
-        EmptyImpl(const SheetInterface& sheet)
-            : Impl(sheet) {}
-
-        Value GetValue() const override {
-            return {};
-        }
-        std::string GetText() const override {
-            return {};
-        }
-        // const std::set<Position> GetReferencedCells() const override {
-        //     return {};
-        // }
-    };
-
-    class TextImpl : public Impl {
-    public:
-        explicit TextImpl(const SheetInterface& sheet, std::string text)
-            : Impl(sheet)
-            , text_(text) {}
-
-        Value GetValue() const override {
-            if(!text_.empty() && text_.at(0) == ESCAPE_SIGN) {
-                return text_.substr(1);
-            }
-            return text_;
-        }
-        std::string GetText() const override {
-            return text_;
-        }
-
-    private:
-        std::string text_;
-    };
-
-    class FormulaImpl : public Impl {
-    public:
-        explicit FormulaImpl(const SheetInterface& sheet, std::unique_ptr<FormulaInterface> formula)
-            : Impl(sheet)
-            , formula_(std::move(formula))
-            // , referenced_cells_(formula_.get()->GetReferencedCells())
-        {}
-
-        Value GetValue() const override {
-            FormulaInterface::Value res = formula_.get()->Evaluate(GetSheet());
-            if (std::holds_alternative<double>(res)) {
-                return std::get<double>(res);
-            } else if (std::holds_alternative<FormulaError>(res)) {
-                return std::get<FormulaError>(res);
-            } else {
-                throw std::runtime_error("e");
-            }
-        }
-
-        std::string GetText() const override {
-            std::string res = "=" + formula_.get()->GetExpression();
-            return res;
-        }
-
-
-        
-    private:
-        std::unique_ptr<FormulaInterface> formula_;
-        std::set<Position> referenced_cells_;
-    };
-
-    std::unique_ptr<Impl> impl_;
-
+        // Добавьте поля и методы для связи с таблицей, проверки циклических 
+        // зависимостей, графа зависимостей и т. д.
     std::vector<Position> referenced_cells_;
     std::vector<Position> dependent_cells_;
+    
 };
