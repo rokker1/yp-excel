7d8ee58eb3ecd787ac77d1ba9e6e1a05c14b1af0
alexander @ubuntu-H310CH5-M2 <darkriderification@yandex.ru>
2022-09-18 16:42:26 +0500

    13 of 15

--- ast/cell.cpp
+++ ast/cell.cpp
@@ -45,7 +45,7 @@ void Cell::Set(std::string text) {
         impl_ = std::make_unique<TextImpl>(sheet_, std::move(text));
     } else {
         // это текст или число!
-        double number = 0.0;
+        [[maybe_unused]] double number = 0.0;
         size_t processed = 0;
         try {
             number = std::stod(text, &processed);
@@ -91,89 +91,3 @@ void Cell::Set(std::string text) {
     std::set<Position> Cell::GetDependentCells() const {
         return impl_->GetDependentCells();
     }
-    // class Cell::Impl {
-    // public:
-    //     Impl(SheetInterface& sheet)
-    //         : sheet_(sheet) {}
-        
-    //     virtual Value GetValue() const = 0;
-    //     virtual std::string GetText() const = 0;
-    //     SheetInterface& GetSheet() const {
-    //         return sheet_;
-    //     }
-
-    //     // virtual const std::set<Position> GetReferencedCells() const = 0;
-
-    // protected:    
-    //     SheetInterface& sheet_;
-
-    //     std::optional<double> cached_value_;
-    // };
-
-    // class Cell::EmptyImpl : public Impl {
-    // public:
-    //     EmptyImpl(SheetInterface& sheet)
-    //         : Impl(sheet) {}
-
-    //     Value GetValue() const override {
-    //         return {};
-    //     }
-    //     std::string GetText() const override {
-    //         return {};
-    //     }
-    //     // const std::set<Position> GetReferencedCells() const override {
-    //     //     return {};
-    //     // }
-    // };
-
-    // class Cell::TextImpl : public Impl {
-    // public:
-    //     explicit TextImpl(SheetInterface& sheet, std::string text)
-    //         : Impl(sheet)
-    //         , text_(text) {}
-
-    //     Value GetValue() const override {
-    //         if(!text_.empty() && text_.at(0) == ESCAPE_SIGN) {
-    //             return text_.substr(1);
-    //         }
-    //         return text_;
-    //     }
-    //     std::string GetText() const override {
-    //         return text_;
-    //     }
-
-    // private:
-    //     std::string text_;
-    // };
-
-    // class Cell::FormulaImpl : public Impl {
-    // public:
-    //     explicit FormulaImpl(SheetInterface& sheet, std::unique_ptr<FormulaInterface> formula)
-    //         : Impl(sheet)
-    //         , formula_(std::move(formula))
-    //         // , referenced_cells_(formula_.get()->GetReferencedCells())
-    //     {}
-
-    //     Value GetValue() const override {
-    //         FormulaInterface::Value res = formula_.get()->Evaluate(sheet_);
-    //         if (std::holds_alternative<double>(res)) {
-    //             return std::get<double>(res);
-    //         } else if (std::holds_alternative<FormulaError>(res)) {
-    //             return std::get<FormulaError>(res);
-    //         } else {
-    //             throw std::runtime_error("e");
-    //         }
-    //         return {};
-    //     }
-
-    //     std::string GetText() const override {
-    //         std::string res = "=" + formula_.get()->GetExpression();
-    //         return res;
-    //     }
-
-
-        
-    // private:
-    //     std::unique_ptr<FormulaInterface> formula_;
-    //     std::set<Position> referenced_cells_;
-    // };
